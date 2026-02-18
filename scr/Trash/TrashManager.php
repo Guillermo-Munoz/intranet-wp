d36bf507d3fdc64cf5ea0e2804630b3704ae7961
Guillermo-Munoz <illloquehaces@hotmail.com>
2026-02-18 16:51:03 +0100

    Mejorado TrashManager para soportar workers

--- scr/Trash/TrashManager.php
+++ scr/Trash/TrashManager.php
@@ -7,28 +7,32 @@ use IntranetGestoria\File\FileSecurity;
 class TrashManager {
     
     public static function moveToTrash($base_path, $file_path, $username) {
-        $trash_path = TrashLogger::getTrashPath($base_path);
-        $timestamp = current_time('timestamp');
-        $date_format = date('Y-m-d_H-i-s', $timestamp);
-        $filename = basename($file_path);
-        
-        if (is_dir($file_path)) {
-            $dest = $trash_path . $date_format . '_' . $filename . '/';
-        } else {
-            $dest = $trash_path . $date_format . '_' . $filename;
-        }
-        
-        // Copiar a papelera
-        FileHandler::copyRecursive($file_path, $dest);
-        
-        // Registrar en log
-        TrashLogger::registerDeletion($base_path, $file_path, is_dir($file_path) ? 'CARPETA' : 'ARCHIVO', $username);
-        
-        // Eliminar original
-        FileHandler::deleteRecursive($file_path);
-        
-        return true;
+    $trash_path = TrashLogger::getTrashPath($base_path);
+    $timestamp = current_time('timestamp');
+    $date_format = date('Y-m-d_H-i-s', $timestamp);
+    $filename = basename($file_path);
+    
+    // Limpiar la ruta para el log (quita la ruta del servidor)
+    $relative_path = str_replace(realpath($base_path) . DIRECTORY_SEPARATOR, '', realpath($file_path));
+    $relative_path = str_replace('\\', '/', $relative_path); // Normalizar barras
+
+    if (is_dir($file_path)) {
+        $dest = $trash_path . $date_format . '_' . $filename . '/';
+    } else {
+        $dest = $trash_path . $date_format . '_' . $filename;
     }
+    
+    // Copiar a papelera
+    FileHandler::copyRecursive($file_path, $dest);
+    
+    // Registrar en log (usando la ruta limpia)
+    TrashLogger::registerDeletion($base_path, $relative_path, is_dir($file_path) ? 'CARPETA' : 'ARCHIVO', $username);
+    
+    // Eliminar original
+    FileHandler::deleteRecursive($file_path);
+    
+    return true;
+}
     
     public static function permanentlyDelete($base_path, $filename) {
         $trash_path = $base_path . INTRANET_TRASH_FOLDER . '/';
